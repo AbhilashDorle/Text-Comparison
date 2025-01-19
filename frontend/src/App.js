0f4596bba0497e7936a64a30eaeaa88edc97739d
Abhilash Dorle <dorleab@gmail.com>
2025-01-18 22:14:23 -0500

    Finished UI

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,16 +4,22 @@ function App() {
   return (
     <div className="App">
       <h1>Text Comparison</h1>
+      <button className="diff-btn">FIND DIFFERENCE</button>
       <div className='text-area-div'>
         <div className='text-area-wrapper'>
-          <button className='upload-btn'>Upload File</button>
-          <textarea className='text-area'>Enter First File Text</textarea>
+          
+          <input type="file" className= "input-field" id="upload-left"/>
+          <textarea className='text-area'></textarea>
+          
         </div>
         <div className='text-area-wrapper'>
-          <button className='upload-btn'>Upload Right</button>
-          <textarea className='text-area'>Enter Second File Text</textarea>
+
+          <input type="file" className= "input-field" id="upload-left"/>
+          <textarea className='text-area'></textarea>
+
         </div>
       </div>
+      
     </div>
   );
 }
